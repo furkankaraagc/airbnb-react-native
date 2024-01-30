3bae4a793adc9af00cdb7b61848d7fa6984f8fd1
Furkan Karaağaç <furkankaraagc@gmail.com>
2024-01-30 17:04:24 +0300

    add listing page and parallax image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,25 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Stack } from 'expo-router';
 import ExploreHeader from '@/components/ExploreHeader';
 import Listings from '@/components/Listings';
+import items from '@/assets/data/airbnb-listings.json';
 
 const Page = () => {
+  const [category, setCategory] = useState<string>('Tiny homes');
+
+  const onDataChanged = (category: string) => {
+    setCategory(category);
+  };
+
   return (
     <View style={{ flex: 1 }}>
-      <Stack.Screen options={{ header: () => <ExploreHeader /> }} />
-      <Listings />
+      <Stack.Screen
+        options={{
+          header: () => <ExploreHeader onCategoryChanged={onDataChanged} />,
+        }}
+      />
+      <Listings items={items} category={category} />
     </View>
   );
 };
--- components/Listings.tsx
+++ components/Listings.tsx
@@ -1,12 +1,115 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ListRenderItem,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from 'react-native';
+import { defaultStyles } from '@/constants/Styles';
+import { Ionicons } from '@expo/vector-icons';
+import { Link } from 'expo-router';
+import Animated, { FadeInRight, FadeOutLeft } from 'react-native-reanimated';
+import { useEffect, useRef, useState } from 'react';
+import {
+  BottomSheetFlatList,
+  BottomSheetFlatListMethods,
+} from '@gorhom/bottom-sheet';
+import items from '@/assets/data/airbnb-listings.json';
 
-const Listings = () => {
+interface Props {
+  items: any[];
+  refresh: number;
+  category: string;
+}
+
+const Listings = ({ items, category }: Props) => {
+  const listRef = useRef<FlatList>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  // Update the view to scroll the list back top
+
+  const scrollListTop = () => {
+    listRef.current?.scrollToOffset({ offset: 0, animated: true });
+  };
+
+  // Use for "updating" the views data after category changed
+  useEffect(() => {
+    console.log(category);
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [category]);
+
+  // Render one listing row for the FlatList
+  const renderRow: ListRenderItem<any> = ({ item }) => (
+    <Link href={`/listing/${item.id}`} asChild>
+      <TouchableOpacity>
+        <Animated.View
+          style={styles.listing}
+          entering={FadeInRight}
+          exiting={FadeOutLeft}
+        >
+          <Image style={styles.image} source={{ uri: item.medium_url }} />
+          <TouchableOpacity
+            style={{ position: 'absolute', right: 30, top: 30 }}
+          >
+            <Ionicons name='heart-outline' size={24} color='white' />
+          </TouchableOpacity>
+          <View
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <Text style={{ fontSize: 16, fontFamily: 'mon-sb' }}>
+              {item.name}
+            </Text>
+            <View style={{ flexDirection: 'row', gap: 4 }}>
+              <Ionicons name='star' size={16} />
+              <Text style={{ fontFamily: 'mon-sb' }}>
+                {item.review_scores_rating / 20}
+              </Text>
+            </View>
+          </View>
+          <Text style={{ fontFamily: 'mon' }}>{item.room_type}</Text>
+          <View style={{ flexDirection: 'row', gap: 4 }}>
+            <Text style={{ fontFamily: 'mon-sb' }}>€ {item.price}</Text>
+            <Text style={{ fontFamily: 'mon' }}>night</Text>
+          </View>
+        </Animated.View>
+      </TouchableOpacity>
+    </Link>
+  );
+
   return (
-    <View>
-      <Text>Listings</Text>
+    <View style={defaultStyles.container}>
+      <FlatList
+        renderItem={renderRow}
+        ref={listRef}
+        data={loading ? [] : items}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listing: {
+    padding: 16,
+    gap: 10,
+    marginVertical: 16,
+  },
+  image: {
+    width: '100%',
+    height: 300,
+    borderRadius: 10,
+  },
+  info: {
+    textAlign: 'center',
+    fontFamily: 'mon-sb',
+    fontSize: 16,
+    marginTop: 4,
+  },
+});
 
 export default Listings;
